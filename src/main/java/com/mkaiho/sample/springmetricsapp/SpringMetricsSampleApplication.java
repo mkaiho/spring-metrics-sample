0c73828d930031c42e08e7bc5445d1f93adcfdad
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2020-06-02 23:53:51 +0900

    Add files generated by Spring initializr

---
+++ src/main/java/com/mkaiho/sample/springmetricsapp/SpringMetricsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.mkaiho.sample.springmetricsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMetricsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMetricsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkaiho/sample/springmetricsapp/SpringMetricsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkaiho.sample.springmetricsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMetricsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
